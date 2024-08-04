c5c1f3b548ceed01c94e19bdeee02f178a39b48d
eri-maulana <maulanabouble11@gmail.com>
2024-08-04 20:09:00 +0700

    01. Instalasi Laravel Filament

---
+++ app/Filament/Resources/PelangganResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PelangganResource\Pages;
+use App\Filament\Resources\PelangganResource\RelationManagers;
+use App\Models\Pelanggan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PelangganResource extends Resource
+{
+    protected static ?string $model = Pelanggan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama'),
+                Forms\Components\TextInput::make('no_hp'),
+                Forms\Components\TextInput::make('alamat'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama'),
+                Tables\Columns\TextColumn::make('no_hp'),
+                Tables\Columns\TextColumn::make('alamat'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPelanggans::route('/'),
+            'create' => Pages\CreatePelanggan::route('/create'),
+            'edit' => Pages\EditPelanggan::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2024_08_04_125653_create_pelanggans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelanggans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('no_hp')->nullable();
+            $table->string('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelanggans');
+    }
+};
