dd74afd6b39562a14f25b3d6dcd75d51e8713404
eri-maulana <maulanabouble11@gmail.com>
2024-08-07 19:07:32 +0700

    05. User Resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -18,23 +18,52 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $label = 'Data User';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\FileUpload::make('avatar_url')
+                    ->required()
+                    ->avatar()
+                    ->label('Avatar'),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->label('Nama'),
+                Forms\Components\TextInput::make('email')
+                    ->label('Email')
+                    ->required(),
+                Forms\Components\TextInput::make('password')
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn (string $context): bool => $context === 'create')
+                    ->label('Kata Sandi')
+                    ->password()
+                    ->revealable(),
+                Forms\Components\Select::make('role')
+                    ->options([
+                        'operator' => 'Operator',
+                        'admin' => 'Admin',
+                    ])
+                    ->required(),
             ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([
-                Tables\Columns\ImageColumn::make('avatar_url')->circular(),
-                Tables\Columns\TextColumn::make('name')->searchable(),
-                Tables\Columns\TextColumn::make('email')->searchable(),
-                Tables\Columns\TextColumn::make('role')->searchable(),
+            ->columns([Tables\Columns\ImageColumn::make('avatar_url')
+                ->circular()
+                ->label('Avatar'),
+            Tables\Columns\TextColumn::make('name')
+                ->label('Nama')
+                ->searchable(),
+            Tables\Columns\TextColumn::make('email')
+                ->label('Email')
+                ->searchable(),
+            Tables\Columns\TextColumn::make('role')
+                ->label('Role')
+                ->searchable(),
                 Tables\Columns\TextColumn::make('created_at'),
             ])
             ->filters([
